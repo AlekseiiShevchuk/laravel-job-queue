693ee4f0acd7377d5e43aa534064f2c1a7b9a307
alekseiishevchuk <shevchuka@gmail.com>
2016-08-08 14:31:57 +0300

    clean event service provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,9 +16,9 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\BookWasCreated' => [
             'App\Listeners\SendEmailNotificationsAboutNewBook',
         ],
-        'App\Events\BookWasAddedToUser' => [
-            'App\Listeners\MakeRefundNotificationJob',
-        ],
+//        'App\Events\BookWasAddedToUser' => [
+//            'App\Listeners\MakeRefundNotificationJob',
+//        ],
     ];
 
     /**
